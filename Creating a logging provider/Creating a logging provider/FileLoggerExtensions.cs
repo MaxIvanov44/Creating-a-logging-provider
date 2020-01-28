eb116b7fa99e2d4da50e2ebb71aac6d55ea05726
MaxIvanov44 <Lvanovmaksim200033@gmail.com>
2020-01-28 14:21:54 +0300

    lastlog

---
+++ Creating a logging provider/Creating a logging provider/FileLoggerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Creating_a_logging_provider
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory,
+                                        string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
---
+++ Creating a logging provider/Creating a logging provider/FileLoggerProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Creating_a_logging_provider
+{
+    public class FileLoggerProvider : ILoggerProvider
+    {
+        private string path;
+
+        public FileLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
